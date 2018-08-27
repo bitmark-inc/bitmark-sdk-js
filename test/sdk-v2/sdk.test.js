69da622f7c300fbbfde5cce6a1d970bbd521d9f7
Dung Le <phieudu241@gmail.com>
2018-08-27 09:12:11 +0700

    Update sdk test cases

--- test/sdk-v2/sdk.test.js
+++ test/sdk-v2/sdk.test.js
@@ -1,22 +1,19 @@
-let chai = require('chai');
-let expect = chai.expect;
-let sdk = require('../../index');
-let Account = sdk.Account;
+const chai = require('chai');
+const expect = chai.expect;
+const sdk = require('../../index');
 
 describe('SDK', function () {
     describe('initialization', function () {
         it('should build with livenet as default', function () {
             sdk.init({});
 
-            let account = new Account();
-            expect(account.getNetwork()).to.equal('livenet');
+            expect(global.getSDKConfig().network).to.equal('livenet');
         });
 
         it('should build for testnet if specified', function () {
             sdk.init({network: 'testnet'});
 
-            let account = new Account();
-            expect(account.getNetwork()).to.equal('testnet');
+            expect(global.getSDKConfig().network).to.equal('testnet');
         });
 
         it('should throw errors on wrong network or version', function () {
