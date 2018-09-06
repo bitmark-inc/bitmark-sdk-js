4b1178f1d7c8d1176d5e0f3382e1c8f8b664ad87
Dung Le <phieudu241@gmail.com>
2018-09-06 15:49:14 +0700

    Remove unused stuffs

--- sdk-v2/service/api-service.js
+++ sdk-v2/service/api-service.js
@@ -2,7 +2,6 @@
 
 const axios = require('axios');
 const _ = require('lodash');
-const FormData = require('form-data');
 
 const NETWORKS_CONFIG = require('../config/network-config');
 const assert = require('../util/assert.js');
@@ -46,44 +45,7 @@ let sendRequest = async (options) => {
     return response.data;
 };
 
-let sendMultipartRequest = async (options) => {
-    common.makeSureSDKInitialized();
-    options = options || {};
-    let apiUrl = options.url;
-    let params = options.params;
-    let headers = options.headers;
-    let network = NETWORKS_CONFIG[global.getSDKConfig().network];
-
-    assert(network, 'unrecognized network', SDKError.INVALID_PARAMETERS_ERROR_CODE);
-    assert(apiUrl, 'missing api url', SDKError.SERVICE_FAILED);
-
-    let url = `${network.api_server}/${network.api_version}/${apiUrl}`;
-
-    let form = new FormData();
-    for (let i in params) {
-        form.append(i, params[i]);
-    }
-
-    let requestOptions = {
-        method: 'post',
-        url: url,
-        data: form,
-        headers: _.merge(headers, form.getHeaders())
-    };
-
-    try {
-        let response = await axios(requestOptions);
-        return response.data;
-    } catch (error) {
-        if (error.response) {
-            assert(false, error.response.data);
-        } else {
-            assert(false, error.message, SDKError.SERVICE_FAILED);
-        }
-    }
-};
 
 module.exports = {
-    sendRequest,
-    sendMultipartRequest
+    sendRequest
 };
