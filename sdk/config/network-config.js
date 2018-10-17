a16bae2e3d1bc8467bd403c167a6a1b3d0dc487c
Dung Le <phieudu241@gmail.com>
2018-10-17 16:29:56 +0700

    Remove unused config

--- sdk/config/network-config.js
+++ sdk/config/network-config.js
@@ -3,7 +3,6 @@ module.exports = {
   livenet: {
     name: 'livenet',
     account_number_value: 0x00,
-    kif_value: 0x00,
     core_value: 0x00,
     api_server: 'https://api.bitmark.com',
     api_version: 'v3'
@@ -11,7 +10,6 @@ module.exports = {
   testnet: {
     name: 'testnet',
     account_number_value: 0x01,
-    kif_value: 0x01,
     core_value: 0x01,
     api_server: 'https://api.test.bitmark.com',
     api_version: 'v3'
